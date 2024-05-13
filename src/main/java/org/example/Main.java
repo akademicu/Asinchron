86e8c405781f05281cc6e22c87167dedcfee6593
ste <mrbarrow@hotmail.co.uk>
2024-05-13 12:10:43 +0100

    feat: ex1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.concurrent.CompletableFuture;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        CompletableFuture<Void> delay = CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+    CompletableFuture<String> greet = CompletableFuture.supplyAsync(() -> "Hello there");
+
+        delay.thenCompose(v -> greet).thenAccept(System.out::println);
+
+        for (int i = 0; i < 200; i++) {
+//            if (i % 2 == 0) {
+                System.out.println(i);
+
+        }
+
     }
 }
